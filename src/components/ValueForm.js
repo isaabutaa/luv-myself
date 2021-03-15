c3e9a62b76bd588ae30076148d7f155f81556784
Isa Abutaa <isaabutaa@gmail.com>
2021-03-14 21:35:21 -0400

    commit

--- src/components/ValueForm.js
+++ src/components/ValueForm.js
@@ -3,21 +3,30 @@ import { useState } from 'react'
 export default function ValueForm(props) {
     const [inputs, setInputs] = useState({ value: "", description: "" })
 
+    function handleSubmit() {
+    }
+
+    function handleChange() {
+
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
                 type="text"
                 placeholder="Your Value/Strength"
                 name="value"
                 value={inputs.value}
+                onChange={handleChange}
             />
             <textarea 
                 type="text"
                 placeholder="Value/Strength description..."
                 name="description"
                 value={inputs.description}
+                onChange={handleChange}
             />
-            <input type="Submit" />
+            <button>Add</button>
         </form>
     )
 }
